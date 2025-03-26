9148e6b2449443b76632e94e85ee7fb90838a5da
javikeee <javikeee.jer@gmail.com>
2025-03-26 18:55:15 +0100

    Develop - Fix dependency

--- src/main/java/com/keee/inventario/config/SecurityConfig.java
+++ src/main/java/com/keee/inventario/config/SecurityConfig.java
@@ -3,39 +3,29 @@ package com.keee.inventario.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     /**
-     *
-     * El securityFilterChain es una configuración que define cómo las solicitudes HTTP deben ser filtradas y gestionadas en términos de seguridad.
-     * Es donde configuras la protección de endpoints, autenticación, autorización y otras políticas de seguridad.
-     * En Spring Security, puedes crear un @Configuration que contenga el filtro de seguridad utilizando securityFilterChain,
-     * lo que te permitirá aplicar reglas de acceso a los endpoints de tu aplicación.
+     * Configuración de seguridad con el filtro de seguridad.
      *
      * @param http
      * @return
      * @throws Exception
      */
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/api/public/**").permitAll() // Rutas públicas
-                .antMatchers("/api/admin/**").hasRole("ADMIN") // Rutas solo para ADMIN
-                .anyRequest().authenticated() // Resto de rutas requieren autenticación
-                .and()
-                .formLogin()
-                .loginPage("/login") // Página de login personalizada
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/public/**").permitAll()  // Rutas públicas
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")  // Rutas solo para ADMIN
+                        .anyRequest().authenticated()  // Resto de rutas requieren autenticación
+                )
+                .httpBasic(withDefaults()); // Para la autenticación básica HTTP
 
         return http.build();
     }
