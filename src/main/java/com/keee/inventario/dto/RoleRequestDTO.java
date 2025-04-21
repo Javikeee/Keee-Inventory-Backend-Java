79a6c57b290ebe48b5b32ab0454b0652d04a50e7
javikeee <javikeee.jer@gmail.com>
2025-04-21 08:38:40 +0200

    KIBJ-6 - Create Roles DTO

--- src/main/java/com/keee/inventario/dto/RoleRequestDTO.java
+++ src/main/java/com/keee/inventario/dto/RoleRequestDTO.java
@@ -2,14 +2,14 @@ package com.keee.inventario.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Builder
-public class RoleDTO {
+public class RoleRequestDTO {
 
-    private Long id;
     private String name;
     private String description;
-    private boolean active;
+    private List<Long> permissionIds;
 
 }
--- src/main/java/com/keee/inventario/dto/RoleResponseDTO.java
+++ src/main/java/com/keee/inventario/dto/RoleResponseDTO.java
@@ -2,14 +2,16 @@ package com.keee.inventario.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Builder
-public class RoleDTO {
+public class RoleResponseDTO {
 
     private Long id;
     private String name;
     private String description;
     private boolean active;
+    private List<PermissionDTO> permissions;
 
 }
